2f8a38da11c54ef77465a39e1e150eb8ce3fcce9
kientrinhvan <kien.trinhvan@hotmail.com>
2017-06-14 10:49:22 +0700

    test error

--- demo/Program.cs
+++ demo/Program.cs
@@ -11,7 +11,7 @@ namespace demo
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello world!");
-            //System.Console.WriteLine("abc");
+            System.Console.WriteLine("abc")
         }
     }
 }
